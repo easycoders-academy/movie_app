9ca6c25f228978cb491beff0ba3401b0b962c5e5
Anton Golosnichenko <golosnichenko@artics.ru>
2021-01-08 05:18:27 +0300

    2.3 Упражняемся с функцией map

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import React from 'react';
 
+const foodILike = [
+  {
+    'id': 1,
+    'name': 'борщ',
+    'image': 'https://2recepta.com/recept/borshh/borshh.jpg'
+  },
+  {
+    'id': 2,
+    'name': 'драники',
+    'image': 'https://cdn.lifehacker.ru/wp-content/uploads/2018/05/Draniki_1527664015.jpg'
+  },
+  {
+    'id': 3,
+    'name': 'стейк',
+    'image': 'https://cdn.lifehacker.ru/wp-content/uploads/2018/05/20-laJfxakov-dlya-prigotovleniya-deJstvitelno-bozhestvennogo-steJka_1526682003-1140x570.jpg'
+  },
+  {
+    'id': 4,
+    'name': 'хинкали', 
+    'image': 'https://vosmarket.ru/uploads/posts/2018-11/1541331854_vosmarket_ru-xinkali-0.jpg'
+  }
+]
+
 function Food({name, picture}){
   return (
     <div>
@@ -9,29 +32,14 @@ function Food({name, picture}){
   );
 }
 
-const foodILike = [
-  {
-    'name': 'борщ',
-    'image': 'https://2recepta.com/recept/borshh/borshh.jpg'
-  },
-  {
-    'name': 'драники',
-    'image': 'https://cdn.lifehacker.ru/wp-content/uploads/2018/05/Draniki_1527664015.jpg'
-  },
-  {
-    'name': 'стейк',
-    'image': 'https://cdn.lifehacker.ru/wp-content/uploads/2018/05/20-laJfxakov-dlya-prigotovleniya-deJstvitelno-bozhestvennogo-steJka_1526682003-1140x570.jpg'
-  },
-  {
-    'name': 'хинкали', 
-    'image': 'https://vosmarket.ru/uploads/posts/2018-11/1541331854_vosmarket_ru-xinkali-0.jpg'
-  }
-]
+function renderFood(dish) {
+  return <Food key={dish.id} name={dish.name} picture={dish.image} />
+}
 
 function App() {
   return (
     <div>
-      {foodILike.map(dish => <Food name={dish.name} picture={dish.image} />)}
+      {foodILike.map(renderFood)}
     </div>
   );
 }
