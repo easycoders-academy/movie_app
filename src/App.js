24380d3d70387707ccc33c76f45defee3bbb9e1b
Anton Golosnichenko <golosnichenko@artics.ru>
2021-01-08 04:18:45 +0300

    2.2 Динамическая генерация компонентов

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
+import React from 'react';
 
-function Food({favourite}){
-  return (<h3>Я люблю {favourite}</h3>);
+function Food({name, picture}){
+  return (
+    <div>
+      <h3>Я люблю {name}</h3>
+      <img src={picture} alt=""/>
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    'name': 'борщ',
+    'image': 'https://2recepta.com/recept/borshh/borshh.jpg'
+  },
+  {
+    'name': 'драники',
+    'image': 'https://cdn.lifehacker.ru/wp-content/uploads/2018/05/Draniki_1527664015.jpg'
+  },
+  {
+    'name': 'стейк',
+    'image': 'https://cdn.lifehacker.ru/wp-content/uploads/2018/05/20-laJfxakov-dlya-prigotovleniya-deJstvitelno-bozhestvennogo-steJka_1526682003-1140x570.jpg'
+  },
+  {
+    'name': 'хинкали', 
+    'image': 'https://vosmarket.ru/uploads/posts/2018-11/1541331854_vosmarket_ru-xinkali-0.jpg'
+  }
+]
 
 function App() {
   return (
     <div>
-      <h1>Привет!!!!!</h1>
-      <Food favourite='борщ' />
-      <Food favourite='бутерброды' />
-      <Food favourite='баранину' />
+      {foodILike.map(dish => <Food name={dish.name} picture={dish.image} />)}
     </div>
   );
 }
