0d5e099ad719b566478024f06fc61bf958f676c3
Anton Golosnichenko <golosnichenko@artics.ru>
2021-01-09 06:25:34 +0300

    4.0 Получение фильмов по API

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 class App extends React.Component {
 
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   };
 
-  add = () => {
-    this.setState(current => ({count: current.count + 1}));
-  }
-
-  minus = () => {
-    this.setState(current => ({count: current.count - 1}));
+  getMovies = async () => {
+    const movies = await axios.get('https://yts.mx/api/v2/list_movies.json');
   }
 
   componentDidMount() {
-    console.log('Компонент отрендерился');
-  }
-
-  componentDidUpdate(){
-    console.log('Компонент обновился');
-  }
-
-  componentWillUnmount(){
-    console.log('Компонент умер :(');
+    this.getMovies();
   }
 
   render() {
-    console.log('Я рендерюсь');
-    return <div>
-      <h1>Текущее число: {this.state.count}</h1>
-      <button onClick={this.add}>Плюс</button>
-      <button onClick={this.minus}>Минус</button>
-    </div>
+    const {isLoading} = this.state;
+    return <div>{isLoading ? "Загрузка..." : "Страница загружена"}</div>
   }
 }
 
