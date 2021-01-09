71f5f0447e232e36eb4940214cbfa20b4a477086
Anton Golosnichenko <golosnichenko@artics.ru>
2021-01-09 15:22:02 +0300

    4.2 Оформление фильмов

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -27,12 +28,16 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? "Загрузка..."
-          : movies.map((movie) => {
-              console.log(movie);
-              return (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Загрузка...</span>
+          </div>
+        ) : (
+          movies.map((movie) => {
+            console.log(movie);
+            return (
+              <div class="movies">
                 <Movie
                   key={movie.id}
                   id={movie.id}
@@ -41,9 +46,11 @@ class App extends React.Component {
                   summary={movie.summary}
                   poster={movie.medium_cover_image}
                 />
-              );
-            })}
-      </div>
+              </div>
+            );
+          })
+        )}
+      </section>
     );
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
 function Movie({ id, year, title, summary, poster }) {
-  return <h4>{title}</h4>;
+  return (
+    <div class="movie">
+      <img src={poster} alt={title} title={title} />
+      <div class="movie__column">
+        <h3 class="movie__title">{title}</h3>
+        <h5 class="movie__year">{year}</h5>
+        <p class="movie__summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 Movie.propTypes = {
